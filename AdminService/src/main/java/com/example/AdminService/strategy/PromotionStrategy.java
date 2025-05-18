efd9acd93719011b26b5504abb85aa1e791c2249
yasmine-elassiouty <yasmineelassiouty@gmail.com>
2025-05-18 19:40:40 +0300

    Remove unneeded files

--- AdminService/src/main/java/com/example/AdminService/strategy/PromotionStrategy.java
+++
@@ -1,7 +0,0 @@
-package com.example.AdminService.strategy;
-
-import com.example.AdminService.model.Product;
-
-public interface PromotionStrategy {
-    double applyPromotion(double originalPrice, Double percentage, Double amount);
-}
