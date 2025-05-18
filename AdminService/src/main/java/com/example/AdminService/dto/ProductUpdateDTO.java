b9e9787e89f6cfbf17fe206ed315046249e82237
JanaKorayem <janakorayem12@gmail.com>
2025-05-18 23:30:35 +0300

    update DTO

---
+++ AdminService/src/main/java/com/example/AdminService/dto/ProductUpdateDTO.java
@@ -0,0 +1,52 @@
+package com.example.AdminService.dto;
+
+public class ProductUpdateDTO {
+    private String name;
+    private Double price;
+    private Integer quantity;
+    private String description;
+    private Integer stockThreshold;
+
+    // Using wrapper classes (Double, Integer) instead of primitives (double, int)
+    // so we can check if a field was included in the request
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public Double getPrice() {
+        return price;
+    }
+
+    public void setPrice(Double price) {
+        this.price = price;
+    }
+
+    public Integer getQuantity() {
+        return quantity;
+    }
+
+    public void setQuantity(Integer quantity) {
+        this.quantity = quantity;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public Integer getStockThreshold() {
+        return stockThreshold;
+    }
+
+    public void setStockThreshold(Integer stockThreshold) {
+        this.stockThreshold = stockThreshold;
+    }
+} 
